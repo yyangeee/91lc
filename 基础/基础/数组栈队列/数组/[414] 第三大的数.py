7a6e52460012b3b81ce85fe16310ecf133ff8c7b
yyangeee <winniezovo@gmail.com>
2021-09-21 09:00:26 +0100

    day1

---
+++ 基础/基础/数组栈队列/数组/[414] 第三大的数.py
@@ -0,0 +1,85 @@
+#
+# @lc app=leetcode.cn id=414 lang=python3
+#
+# [414] 第三大的数
+#
+# https://leetcode-cn.com/problems/third-maximum-number/description/
+#
+# algorithms
+# Easy (36.40%)
+# Likes:    246
+# Dislikes: 0
+# Total Accepted:    60.3K
+# Total Submissions: 165.8K
+# Testcase Example:  '[3,2,1]'
+#
+# 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：[3, 2, 1]
+# 输出：1
+# 解释：第三大的数是 1 。
+# 
+# 示例 2：
+# 
+# 
+# 输入：[1, 2]
+# 输出：2
+# 解释：第三大的数不存在, 所以返回最大的数 2 。
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：[2, 2, 3, 1]
+# 输出：1
+# 解释：注意，要求返回第三大的数，是指在所有不同数字中排第三大的数。
+# 此例中存在两个值为 2 的数，它们都排第二。在所有不同数字中排第三大的数为 1 。
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 
+# -2^31 
+# 
+# 
+# 
+# 
+# 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？
+# 
+#
+
+# @lc code=start
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first = float("-inf")
+        second = float("-inf")
+        third = float("-inf")
+
+        for c in nums:
+            if c == first or c == second or c == third:
+                continue
+            if c > first:
+                third = second
+                second = first
+                first = c
+            elif c > second:
+                third = second
+                second = c
+            elif c > third:
+                third = c
+        if third == float("-inf"):
+            return max(first,second)
+        else:
+            return third
+
+
+
+# @lc code=end
+
