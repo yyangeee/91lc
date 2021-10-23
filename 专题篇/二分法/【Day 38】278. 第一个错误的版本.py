76d57124677950812ae582e0ea532cdb26ab5bfe
yyangeee <winniezovo@gmail.com>
2021-10-23 14:07:53 +0800

    day7

---
+++ 专题篇/二分法/【Day 38】278. 第一个错误的版本.py
@@ -0,0 +1,77 @@
+#
+# @lc app=leetcode.cn id=278 lang=python3
+#
+# [278] 第一个错误的版本
+#
+# https://leetcode-cn.com/problems/first-bad-version/description/
+#
+# algorithms
+# Easy (45.46%)
+# Likes:    435
+# Dislikes: 0
+# Total Accepted:    191.4K
+# Total Submissions: 422.1K
+# Testcase Example:  '5\n4'
+#
+# 
+# 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+# 
+# 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+# 
+# 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version
+# 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：n = 5, bad = 4
+# 输出：4
+# 解释：
+# 调用 isBadVersion(3) -> false 
+# 调用 isBadVersion(5) -> true 
+# 调用 isBadVersion(4) -> true
+# 所以，4 是第一个错误的版本。
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：n = 1, bad = 1
+# 输出：1
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 
+# 
+# 
+#
+
+# @lc code=start
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l = 0
+        r = n
+        while l<r:
+            mind = int((r-l)/2+l)
+            if isBadVersion(mind):
+                r = mind
+            else:
+                l = mind +1
+        return r
+        
+# @lc code=end
+
